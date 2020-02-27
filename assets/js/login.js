627599bb332e2d4320a95a210b7ff1d1b460e52d
liuqingyi <1152554865@qq.com>
2020-02-27 19:24:55 +0800

    登录注册完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,7 @@
 $(function () {
   // 只要导入了 layui.all.js 脚本，就可以使用 layui.form
   var form = layui.form
+  var layer = layui.layer
 
   // 点击了注册的链接
   $('#link-reg').on('click', function () {
@@ -28,5 +29,57 @@ $(function () {
         return '两次的密码不一致！'
       }
     }
+  })
+
+  // 监听注册表单的提交事件
+  $('#form-reg').on('submit', function (e) {
+    // 1. 阻止表单的默认提交行为
+    e.preventDefault()
+    // 2. 发起Ajax请求
+    $.ajax({
+      // 指定请求的方式
+      type: 'POST',
+      // 指定请求的 URL 地址
+      // 注意：必须是 【请求根路径】 拼接上 【具体的 URL 地址】
+      url: 'http://www.liulongbin.top:3007/api/reguser',
+      // 指定请求的数据
+      data: $(this).serialize(),
+      // 指定成功的回调函数
+      success: function (res) {
+        if (res.status !== 0) {
+          // 注册失败
+          return layer.msg(res.message)
+        }
+        // 如果没有被 return 出去，证明注册成功
+        layer.msg('注册成功，请登录！')
+        // 主动切换到登录的表单
+        $('#link-login').click()
+      }
+    })
+  })
+
+  // 监听登录表单的提交事件
+  $('#form-login').on('submit', function (e) {
+    // 1. 阻止默认提交行为
+    e.preventDefault()
+    // 2. 手动发起 ajax 请求
+    $.ajax({
+      type: 'POST',
+      url: 'http://www.liulongbin.top:3007/api/login',
+      data: $(this).serialize(),
+      success: function (res) {
+        // 先使用 if 判断失败的请求，然后 return 出去
+        // 如果没有被 return 出去，那就是成功了
+        if (res.status !== 0) {
+          return layer.msg('登录失败！')
+        }
+        // 提示用户登录成功
+        layer.msg('登录成功！')
+        // 将服务器颁发的 token 字符串，持久化存储到 localStorage
+        localStorage.setItem('token', res.token)
+        // 跳转到后台首页
+        location.href = '/index.html'
+      }
+    })
   })
 })
